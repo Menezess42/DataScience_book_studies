2f1ed5e1007c6205a3802f07c09e4690bacd467e
Menezess42 <arielmenezess@hotmail.com>
2025-05-27 20:09:58 -0300

    Try to deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,55 +1,61 @@
 {
-  description = "Projeto que estende o ambiente Essentials";
+    description = "Projeto que estende o ambiente Essentials";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+    };
 
-  outputs = { self, nixpkgs, flake-utils, essentials }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # Overlay local que desativa completamente o checkPhase em pacotes Python
-        pythonOverlay = final: prev: {
-          python311 = prev.python311.override {
-            packageOverrides = pyself: pysuper: {
-              buildPythonPackage = args: pysuper.buildPythonPackage (args // {
-                doCheck = false;
-                checkInputs = [];
-                nativeCheckInputs = [];
-                checkPhase = ''
-                  echo "[checkPhase] Desativado por overlay do flake."
-                '';
-              });
-            };
-          };
-        };
+    outputs = { self, nixpkgs, flake-utils, essentials }:
+        flake-utils.lib.eachDefaultSystem (system:
+                let
+# Overlay local que desativa o checkPhase nos pacotes Python
+                pythonOverlay = final: prev: {
+                python311 = prev.python311.override {
+                packageOverrides = pyself: pysuper: {
+                buildPythonPackage = args: pysuper.buildPythonPackage (args // {
+                        doCheck = false;
+                        checkPhase = "echo 'checkPhase desativado por flake.'";
+                        });
+                };
+                };
+                };
 
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ pythonOverlay ];
-        };
+                pkgs = import nixpkgs {
+                inherit system;
+                overlays = [ 
+                (final: prev: {
+                 python311 = prev.python311.override {
+                 packageOverrides = pyself: pysuper: {
+                 buildPythonPackage = args: pysuper.buildPythonPackage (args // {
+                         doCheck = false;
+                         checkInputs = [];
+                         nativeCheckInputs = [];
+                         checkPhase = ''
+                         echo "[checkPhase] skipping tests (disable by overlay)."
+                         '';
+                         }); }; }; }) ];
+                };
 
-        baseShell = essentials.devShells.${system}.python;
+                baseShell = essentials.devShells.${system}.python;
 
-      in {
-        devShell = pkgs.mkShell {
-          name = "projeto-com-requests";
+                in {
+                    devShell = pkgs.mkShell {
+                        name = "projeto-com-requests";
 
-          buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
-            requests
-            # opencv4  # descomente se necessário
-          ]);
+                        buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
+                                requests
+# opencv4  # descomentável se necessário
+                        ]);
 
-          shellHook = ''
-            echo "Ambiente do projeto carregado (base Essentials + customizações)."
-            ${baseShell.shellHook or ""}
-          '';
-        };
-      });
+                        shellHook = ''
+                            echo "Ambiente do projeto carregado (base Essentials + customizações)."
+                            ${baseShell.shellHook or ""}
+                        '';
+                    };
+                });
 }
-
 #
 # {
 #     description = "Projeto que estende o ambiente Essentials";
