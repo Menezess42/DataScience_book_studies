dc5bcc998f6efd5dc51088fe3935c841a1e6506c
Menezess42 <arielmenezess@hotmail.com>
2025-05-27 20:24:18 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,29 +1,50 @@
+
 {
-    description = "Projeto que estende o ambiente Essentials";
+  description = "Projeto que estende o ambiente Essentials, sem pytestCheckPhase";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-        essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-    };
+  inputs = {
+    nixpkgs.url       = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url   = "github:numtide/flake-utils";
+    essentials.url    = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+  };
 
-    outputs = { self, nixpkgs, flake-utils, essentials }:
-        flake-utils.lib.eachDefaultSystem (system:
-                let
-                pkgs = import nixpkgs { inherit system; };
-                baseShell = essentials.devShells.${system}.python;
-                in {
-                devShell = pkgs.mkShell {
-                name = "projeto-com-requests";
+  outputs = { self, nixpkgs, flake-utils, essentials }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # 1) Define o overlay que desliga todos os checks nos pacotes Python:
+        disablePythonChecks = final: prev: {
+          buildPythonPackage = prev.buildPythonPackage.overrideAttrs(old: {
+            doCheck   = false;
+            checkPhase = ''
+              echo "## pytestCheckPhase SKIPPED ##"
+            '';
+          });
+        };
 
-                buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
-# opencv4
-                ]);
+        # 2) Importa nixpkgs aplicando o overlay:
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ disablePythonChecks ];
+          # opcional: config.doCheck = false;  # reforço global, mas não obrigatório com o overlay
+        };
 
-                shellHook = ''
-                echo "Ambiente do projeto carregado (base Essentials + customizações)."
-                ${baseShell.shellHook or ""}
-                '';
-                };
-                });
-} 
+        # 3) Puxa o devShell do seu flake “essentials”
+        baseShell = essentials.devShells.${system}.python;
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "projeto-com-requests";
+
+          buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
+            opencv4
+            # … outros pacotes Python
+          ]);
+
+          shellHook = ''
+            echo "Ambiente do projeto carregado (base Essentials + customizações)."
+            ${baseShell.shellHook or ""}
+          '';
+        };
+      }
+    );
+}
