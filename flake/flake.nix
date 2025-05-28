06f26bdb43464220abe446f8084bbc274ef63783
Menezess42 <arielmenezess@hotmail.com>
2025-05-28 10:29:02 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,161 +1,30 @@
-# {
-#     description = "Projeto que estende o ambiente Essentials";
-#
-#     inputs = {
-#         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-#         flake-utils.url = "github:numtide/flake-utils";
-#         essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-#     };
-#
-#     outputs = { self, nixpkgs, flake-utils, essentials }:
-#         flake-utils.lib.eachDefaultSystem (system:
-#                 let
-#                 pkgs = import nixpkgs { inherit system;  config.doCheck = false;};
-#                 baseShell = essentials.devShells.${system}.python;
-#                 in {
-#                 devShell = pkgs.mkShell {
-#                 name = "projeto-com-requests";
-#                 buildInputs = baseShell.buildInputs ++ [ #(with pkgs.python311Packages; [
-#                 (pkgs.python311Packages.seaborn.overrideAttrs (old: {
-#                                                                doCheck = false;
-#                                                                pytestCheckPhase="";
-#                                                                checkPhase = "";
-#                                                                }))
-#
-#                 (pkgs.python311Packages.numpy.overrideAttrs (old: {
-#                                                                doCheck = false;
-#                                                                pytestCheckPhase="";
-#                                                                checkPhase = "";
-#                                                                }))
-#                 (pkgs.python311Packages.matplotlib.overrideAttrs (old: {
-#                                                                doCheck = false;
-#                                                                pytestCheckPhase="";
-#                                                                checkPhase = "";
-#                                                                }))
-#                 ];
-#
-#                 shellHook = ''
-#                 echo "Ambiente do projeto carregado (base Essentials + customizações)."
-#                 ${baseShell.shellHook or ""}
-#                 '';
-#                 };
-#                 });
-# } 
-# {
-#   description = "Projeto que estende o ambiente Essentials e usa venv";
-#
-#   inputs = {
-#     nixpkgs.url    = "github:NixOS/nixpkgs/nixpkgs-unstable";
-#     flake-utils.url = "github:numtide/flake-utils";
-#     essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-#   };
-#
-#   outputs = { self, nixpkgs, flake-utils, essentials }:
-#     flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         pkgs      = import nixpkgs { inherit system; };
-#         pythonPkgs = pkgs.python311Packages;
-#         baseShell  = essentials.devShells.${system}.python;
-#       in {
-#         devShell = pkgs.mkShell rec {
-#           name    = "projeto-com-venv";
-#           # Diretório do venv
-#           venvDir = ".venv";
-#
-#           buildInputs = [
-#             # Interpretador Python com suporte a venv
-#             pythonPkgs.python
-#
-#             # Hook que cria e ativa o .venv antes de cair no shell
-#             pythonPkgs.venvShellHook
-#
-#             # Dependências leves que queremos derivar pelo Nix
-#             pythonPkgs.numpy
-#
-#             # Dependências do essentials (LSP, debug etc.)
-#           ] ++ baseShell.buildInputs;
-#
-#           # Se precisar de libs nativas para compilar extensões pip
-#           nativeBuildInputs = with pkgs; [
-#             openssl
-#             zlib
-#             libxml2
-#             libxslt
-#             libzip
-#           ];
-#
-#           # Após criar o .venv, instala tudo do requirements.txt via pip
-#           postVenvCreation = ''
-#             unset SOURCE_DATE_EPOCH
-#             pip install --upgrade pip
-#             pip install -r requirements.txt
-#           '';
-#
-#           # Ajustes após entrar no shell
-#           postShellHook = ''
-#             # Exportar variável se necessário em runtime
-#             echo "Ambiente (Essentials + venv Python) pronto."
-#           '';
-#
-#           # Mantém seu shellHook original do Essentials
-#           shellHook = ''
-#           export NIX_BUILD_CORES=2
-#             ${baseShell.shellHook or ""}
-#           '';
-#         };
-#       });
-# }
 {
-  description = "Projeto que estende o ambiente Essentials e usa venv corretamente";
+    description = "Projeto que estende o ambiente Essentials";
 
-  inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    essentials.url  = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+    };
 
-  outputs = { self, nixpkgs, flake-utils, essentials }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs   = import nixpkgs { inherit system; };
-        python = pkgs.python311;
-        baseShell = essentials.devShells.${system}.python;
-      in {
-        # ⬅️ aqui
-        devShells = {
-          default = pkgs.mkShell rec {
-            name    = "projeto-com-venv";
-            venvDir = ".venv";  # onde o hook vai criar o virtual-env
+    outputs = { self, nixpkgs, flake-utils, essentials }:
+        flake-utils.lib.eachDefaultSystem (system:
+                let
+                pkgs = import nixpkgs { inherit system; };
+                baseShell = essentials.devShells.${system}.python;
+                in {
+                devShell = pkgs.mkShell {
+                name = "projeto-com-requests";
 
-            buildInputs = [
-              # o Python “empacotado” com o hook de venv + numpy
-              (python.withPackages (ps: [ ps.venvShellHook ps.numpy ]))
-            ] ++ baseShell.buildInputs;
+                buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
+# opencv4
+                ]);
 
-            nativeBuildInputs = with pkgs; [
-              openssl
-              zlib
-              libxml2
-              libxslt
-              libzip
-            ];
-
-            # roda _depois_ do `python -m venv .venv && source .venv/bin/activate`
-            postVenvCreation = ''
-              echo "Criando e configurando o ambiente virtual…"
-              unset SOURCE_DATE_EPOCH
-              pip install --upgrade pip
-              pip install -r requirements.txt
-            '';
-
-            # roda _após_ o postVenvCreation, antes do prompt
-            postShellHook = ''
-              echo "Ambiente (Essentials + venv Python) pronto."
-              ${baseShell.shellHook or ""}
-            '';
-
-            # NÃO defina shellHook aqui, para não sobrescrever o hook do venvShellHook
-          };
-        };
-      });
-}
+                shellHook = ''
+                export NIX_BUILD_CORES=3
+                echo "Ambiente do projeto carregado (base Essentials + customizações)."
+                ${baseShell.shellHook or ""}
+                '';
+                };
+                });
+} 
