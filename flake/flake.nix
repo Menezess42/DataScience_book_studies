48fdbbe62b07ae1af6735f1f8864878f9b3b375e
Menezess42 <arielmenezess@hotmail.com>
2025-05-28 09:59:17 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -41,3 +41,69 @@
                 };
                 });
 } 
+
+
+{
+  description = "Projeto que estende o ambiente Essentials e usa venv";
+
+  inputs = {
+    nixpkgs.url    = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, essentials }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs      = import nixpkgs { inherit system; };
+        pythonPkgs = pkgs.python311Packages;
+        baseShell  = essentials.devShells.${system}.python;
+      in {
+        devShell = pkgs.mkShell rec {
+          name    = "projeto-com-venv";
+          # Diretório do venv
+          venvDir = ".venv";
+
+          buildInputs = [
+            # Interpretador Python com suporte a venv
+            pythonPkgs.python
+
+            # Hook que cria e ativa o .venv antes de cair no shell
+            pythonPkgs.venvShellHook
+
+            # Dependências leves que queremos derivar pelo Nix
+            pythonPkgs.numpy
+
+            # Dependências do essentials (LSP, debug etc.)
+          ] ++ baseShell.buildInputs;
+
+          # Se precisar de libs nativas para compilar extensões pip
+          nativeBuildInputs = with pkgs; [
+            openssl
+            zlib
+            libxml2
+            libxslt
+            libzip
+          ];
+
+          # Após criar o .venv, instala tudo do requirements.txt via pip
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+            pip install --upgrade pip
+            pip install -r requirements.txt
+          '';
+
+          # Ajustes após entrar no shell
+          postShellHook = ''
+            # Exportar variável se necessário em runtime
+            echo "Ambiente (Essentials + venv Python) pronto."
+          '';
+
+          # Mantém seu shellHook original do Essentials
+          shellHook = ''
+          export NIX_BUILD_CORES=2
+            ${baseShell.shellHook or ""}
+          '';
+        };
+      });
+}
