f99ceb816818631c68cfd1a68ecef8f6149c27f9
Menezess42 <arielmenezess@hotmail.com>
2025-05-28 11:07:07 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,49 +1,51 @@
 {
-  description = "Projeto Python com venv em flake";
+    description = "Projeto Python com venv em flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+    };
 
-  outputs = { self, nixpkgs, flake-utils, essentials }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        #python = pkgs.python311;
-        pythonPkgs = pkgs.python311Packages;
-        baseShell = essentials.devShells.${system}.python;
-      in {
-        devShell = pkgs.mkShell rec {
-          name = "impurePythonEnv-flake";
-          venvDir = "./.venv";
+    outputs = { self, nixpkgs, flake-utils, essentials }:
+        flake-utils.lib.eachDefaultSystem (system:
+                let
+                pkgs = import nixpkgs { inherit system; };
+#python = pkgs.python311;
+                pythonPkgs = pkgs.python311Packages;
+                baseShell = essentials.devShells.${system}.python;
+                in {
+                devShell = pkgs.mkShell rec {
+                name = "impurePythonEnv-flake";
+                venvDir = "./.venv";
 
-          buildInputs =[
-              pythonPkgs.python
-                  pythonPkgs.venvShellHook
-                  pythonPkgs.numpy
-                  pythonPkgs.requests
-                  pkgs.taglib
-                  pkgs.openssl
-                  pkgs.libxml2
-                  pkgs.libxslt
-                  pkgs.libzip
-                  pkgs.zlib
-                  pkgs.git
-          ] ++ baseShell.buildInputs;
+                buildInputs =[
+                pythonPkgs.python
+                pythonPkgs.venvShellHook
+                pythonPkgs.numpy
+                pythonPkgs.requests
+                pythonPkgs.seaborn
+                pkgs.taglib
+                pkgs.openssl
+                pkgs.libxml2
+                pkgs.libxslt
+                pkgs.libzip
+                pkgs.zlib
+                pkgs.git
+                ] ++ baseShell.buildInputs;
 
-          # Install pip dependencies into the venv
-          postVenvCreation = ''
-            unset SOURCE_DATE_EPOCH
-            pip install -r requirements.txt
-          '';
+# Install pip dependencies into the venv
+                postVenvCreation = ''
+                    export NIX_BUILD_CORES=3
+                    unset SOURCE_DATE_EPOCH
+                    pip install -r requirements.txt
+                    '';
 
-          # Allow pip install wheels
-          postShellHook = ''
-            unset SOURCE_DATE_EPOCH
-          '';
-        };
-      }
-    );
+# Allow pip install wheels
+                postShellHook = ''
+                    unset SOURCE_DATE_EPOCH
+                    '';
+                };
+                }
+                );
 }
