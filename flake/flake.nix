1c6394aa2a6171af357fea9def222b42728f06ce
Menezess42 <arielmenezess@hotmail.com>
2025-05-27 21:44:11 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -4,18 +4,19 @@
   inputs = {
     nixpkgs.url     = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    essentials.url  = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+    essentials.url  = "git+file:///…/flakeEssentials";
   };
 
   outputs = { self, nixpkgs, flake-utils, essentials }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        # overlay que desativa todos os pytest nos python311Packages
         disableAllPythonChecks = final: prev: {
           python311Packages = prev.python311Packages.overrideScope (self: super: {
             inherit (builtins.listToAttrs
               (map (p: {
-                name = p;
-                value = super.${p}.overrideAttrs (_: { doCheck = false; });
+                name  = p;
+                value = super.${p}.overrideAttrs (old: { doCheck = false; });
               })
               (builtins.attrNames super)
             ));
@@ -24,76 +25,25 @@
 
         pkgs = import nixpkgs {
           inherit system;
-          config.doCheck = false;
+          config.doCheck = false;       # ainda bom manter
           overlays = [ disableAllPythonChecks ];
         };
 
+        # agora esse baseShell já vem com pytest desligado em todo python311Packages
         baseShell = essentials.devShells.${system}.python;
-
       in {
         devShell = pkgs.mkShell {
           name = "projeto-com-requests";
-
-          # Aqui você mantém a concatenação normalmente
-          buildInputs = baseShell.buildInputs ++ (with pkgs.python311Packages; [
-            requests
-            # opencv4  <- quando quiser usar, descomenta
-          ]);
-
-          shellHook = ''
-            echo "Ambiente do projeto carregado (base Essentials + customizações)."
+          buildInputs = baseShell.buildInputs;
+          shellHook   = ''
+            echo "Ambiente carregado!"
             ${baseShell.shellHook or ""}
           '';
         };
-      });
+      }
+    );
 }
 
-# {
-#   description = "Projeto que estende o ambiente Essentials";
-#
-#   inputs = {
-#     nixpkgs.url     = "github:NixOS/nixpkgs/nixpkgs-unstable";
-#     flake-utils.url = "github:numtide/flake-utils";
-#     essentials.url  = "git+file:///…/flakeEssentials";
-#   };
-#
-#   outputs = { self, nixpkgs, flake-utils, essentials }:
-#     flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         # overlay que desativa todos os pytest nos python311Packages
-#         disableAllPythonChecks = final: prev: {
-#           python311Packages = prev.python311Packages.overrideScope (self: super: {
-#             inherit (builtins.listToAttrs
-#               (map (p: {
-#                 name  = p;
-#                 value = super.${p}.overrideAttrs (old: { doCheck = false; });
-#               })
-#               (builtins.attrNames super)
-#             ));
-#           });
-#         };
-#
-#         pkgs = import nixpkgs {
-#           inherit system;
-#           config.doCheck = false;       # ainda bom manter
-#           overlays = [ disableAllPythonChecks ];
-#         };
-#
-#         # agora esse baseShell já vem com pytest desligado em todo python311Packages
-#         baseShell = essentials.devShells.${system}.python;
-#       in {
-#         devShell = pkgs.mkShell {
-#           name = "projeto-com-requests";
-#           buildInputs = baseShell.buildInputs;
-#           shellHook   = ''
-#             echo "Ambiente carregado!"
-#             ${baseShell.shellHook or ""}
-#           '';
-#         };
-#       }
-#     );
-# }
-#
 # {
 #     description = "Projeto que estende o ambiente Essentials";
 #
