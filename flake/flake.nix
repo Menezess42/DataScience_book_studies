3808742ef94633a7e0738c2955f9309a256b3ea8
Menezess42 <arielmenezess@hotmail.com>
2025-05-28 10:03:11 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,48 +1,46 @@
-{
-    description = "Projeto que estende o ambiente Essentials";
-
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-        essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
-    };
-
-    outputs = { self, nixpkgs, flake-utils, essentials }:
-        flake-utils.lib.eachDefaultSystem (system:
-                let
-                pkgs = import nixpkgs { inherit system;  config.doCheck = false;};
-                baseShell = essentials.devShells.${system}.python;
-                in {
-                devShell = pkgs.mkShell {
-                name = "projeto-com-requests";
-                buildInputs = baseShell.buildInputs ++ [ #(with pkgs.python311Packages; [
-                (pkgs.python311Packages.seaborn.overrideAttrs (old: {
-                                                               doCheck = false;
-                                                               pytestCheckPhase="";
-                                                               checkPhase = "";
-                                                               }))
-                                                               
-                (pkgs.python311Packages.numpy.overrideAttrs (old: {
-                                                               doCheck = false;
-                                                               pytestCheckPhase="";
-                                                               checkPhase = "";
-                                                               }))
-                (pkgs.python311Packages.matplotlib.overrideAttrs (old: {
-                                                               doCheck = false;
-                                                               pytestCheckPhase="";
-                                                               checkPhase = "";
-                                                               }))
-                ];
-
-                shellHook = ''
-                echo "Ambiente do projeto carregado (base Essentials + customizações)."
-                ${baseShell.shellHook or ""}
-                '';
-                };
-                });
-} 
-
-
+# {
+#     description = "Projeto que estende o ambiente Essentials";
+#
+#     inputs = {
+#         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#         flake-utils.url = "github:numtide/flake-utils";
+#         essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+#     };
+#
+#     outputs = { self, nixpkgs, flake-utils, essentials }:
+#         flake-utils.lib.eachDefaultSystem (system:
+#                 let
+#                 pkgs = import nixpkgs { inherit system;  config.doCheck = false;};
+#                 baseShell = essentials.devShells.${system}.python;
+#                 in {
+#                 devShell = pkgs.mkShell {
+#                 name = "projeto-com-requests";
+#                 buildInputs = baseShell.buildInputs ++ [ #(with pkgs.python311Packages; [
+#                 (pkgs.python311Packages.seaborn.overrideAttrs (old: {
+#                                                                doCheck = false;
+#                                                                pytestCheckPhase="";
+#                                                                checkPhase = "";
+#                                                                }))
+#
+#                 (pkgs.python311Packages.numpy.overrideAttrs (old: {
+#                                                                doCheck = false;
+#                                                                pytestCheckPhase="";
+#                                                                checkPhase = "";
+#                                                                }))
+#                 (pkgs.python311Packages.matplotlib.overrideAttrs (old: {
+#                                                                doCheck = false;
+#                                                                pytestCheckPhase="";
+#                                                                checkPhase = "";
+#                                                                }))
+#                 ];
+#
+#                 shellHook = ''
+#                 echo "Ambiente do projeto carregado (base Essentials + customizações)."
+#                 ${baseShell.shellHook or ""}
+#                 '';
+#                 };
+#                 });
+# } 
 {
   description = "Projeto que estende o ambiente Essentials e usa venv";
 
