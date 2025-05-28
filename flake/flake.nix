c267dc2a4566d7ff9db20b51ac782fc3222967e3
Menezess42 <arielmenezess@hotmail.com>
2025-05-28 10:16:56 -0300

    deactivate pycheck

--- flake/flake.nix
+++ flake/flake.nix
@@ -41,11 +41,81 @@
 #                 };
 #                 });
 # } 
+# {
+#   description = "Projeto que estende o ambiente Essentials e usa venv";
+#
+#   inputs = {
+#     nixpkgs.url    = "github:NixOS/nixpkgs/nixpkgs-unstable";
+#     flake-utils.url = "github:numtide/flake-utils";
+#     essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
+#   };
+#
+#   outputs = { self, nixpkgs, flake-utils, essentials }:
+#     flake-utils.lib.eachDefaultSystem (system:
+#       let
+#         pkgs      = import nixpkgs { inherit system; };
+#         pythonPkgs = pkgs.python311Packages;
+#         baseShell  = essentials.devShells.${system}.python;
+#       in {
+#         devShell = pkgs.mkShell rec {
+#           name    = "projeto-com-venv";
+#           # Diretório do venv
+#           venvDir = ".venv";
+#
+#           buildInputs = [
+#             # Interpretador Python com suporte a venv
+#             pythonPkgs.python
+#
+#             # Hook que cria e ativa o .venv antes de cair no shell
+#             pythonPkgs.venvShellHook
+#
+#             # Dependências leves que queremos derivar pelo Nix
+#             pythonPkgs.numpy
+#
+#             # Dependências do essentials (LSP, debug etc.)
+#           ] ++ baseShell.buildInputs;
+#
+#           # Se precisar de libs nativas para compilar extensões pip
+#           nativeBuildInputs = with pkgs; [
+#             openssl
+#             zlib
+#             libxml2
+#             libxslt
+#             libzip
+#           ];
+#
+#           # Após criar o .venv, instala tudo do requirements.txt via pip
+#           postVenvCreation = ''
+#             unset SOURCE_DATE_EPOCH
+#             pip install --upgrade pip
+#             pip install -r requirements.txt
+#           '';
+#
+#           # Ajustes após entrar no shell
+#           postShellHook = ''
+#             # Exportar variável se necessário em runtime
+#             echo "Ambiente (Essentials + venv Python) pronto."
+#           '';
+#
+#           # Mantém seu shellHook original do Essentials
+#           shellHook = ''
+#           export NIX_BUILD_CORES=2
+#             ${baseShell.shellHook or ""}
+#           '';
+#         };
+#       });
+# }
+
+
+
+
+
+
 {
-  description = "Projeto que estende o ambiente Essentials e usa venv";
+  description = "Projeto que estende o ambiente Essentials e usa venv corretamente";
 
   inputs = {
-    nixpkgs.url    = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     essentials.url = "git+file:///mnt/hdmenezess42/GitProjects/flakeEssentials";
   };
@@ -53,29 +123,19 @@
   outputs = { self, nixpkgs, flake-utils, essentials }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs      = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python311;
         pythonPkgs = pkgs.python311Packages;
-        baseShell  = essentials.devShells.${system}.python;
+        baseShell = essentials.devShells.${system}.python;
       in {
-        devShell = pkgs.mkShell rec {
-          name    = "projeto-com-venv";
-          # Diretório do venv
-          venvDir = ".venv";
+        devShell = pkgs.mkShell {
+          name = "projeto-com-venv";
 
           buildInputs = [
-            # Interpretador Python com suporte a venv
-            pythonPkgs.python
-
-            # Hook que cria e ativa o .venv antes de cair no shell
-            pythonPkgs.venvShellHook
-
-            # Dependências leves que queremos derivar pelo Nix
-            pythonPkgs.numpy
-
-            # Dependências do essentials (LSP, debug etc.)
+            # Cria Python com o hook de venv ativado
+            (python.withPackages (ps: [ ps.venvShellHook ps.numpy ]))
           ] ++ baseShell.buildInputs;
 
-          # Se precisar de libs nativas para compilar extensões pip
           nativeBuildInputs = with pkgs; [
             openssl
             zlib
@@ -84,24 +144,40 @@
             libzip
           ];
 
-          # Após criar o .venv, instala tudo do requirements.txt via pip
+          venvDir = ".venv";
+
           postVenvCreation = ''
+            echo "Criando e configurando o ambiente virtual..."
             unset SOURCE_DATE_EPOCH
             pip install --upgrade pip
             pip install -r requirements.txt
           '';
 
-          # Ajustes após entrar no shell
           postShellHook = ''
-            # Exportar variável se necessário em runtime
             echo "Ambiente (Essentials + venv Python) pronto."
           '';
 
-          # Mantém seu shellHook original do Essentials
           shellHook = ''
-          export NIX_BUILD_CORES=2
+            export NIX_BUILD_CORES=2
             ${baseShell.shellHook or ""}
           '';
         };
       });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
